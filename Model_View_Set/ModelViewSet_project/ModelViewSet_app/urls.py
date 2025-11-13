807f070d7cddafa441f19e08b72e1d2ac414ff97
Muhammed Fahiz M <muhammedfahiz777145@gmail.com>
2025-11-13 16:57:52 +0530

    model view set

--- Model_View_Set/ModelViewSet_project/ModelViewSet_app/urls.py
+++ Model_View_Set/ModelViewSet_project/ModelViewSet_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register(r'tasks',TaskViewSet,basename='task')
+
+urlpatterns = router.urls
+
--- Model_View_Set/ModelViewSet_project/ModelViewSet_app/views.py
+++ Model_View_Set/ModelViewSet_project/ModelViewSet_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Task
+from .serializers import TaskSerialzer
 # Create your views here.
+
+# class TaskViewSet(ModelViewSet):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerialzer
+
+
+
+# Customized : added filter options.
+
+
+# class TaskViewSet(ModelViewSet):
+#     serializer_class = TaskSerialzer
+
+#     def get_queryset(self):
+#         return Task.objects.filter(completed = True) # only returns incomplet tasks.
+
+
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerialzer
+
+    @action(detail=False,methods=['get'])
+    def completed(self,request):
+        completed_tasks = Task.objects.filter(completed = True)
+        serializer = self.get_serializer(completed_tasks,many = True)
+        return Response(serializer.data) 
+
