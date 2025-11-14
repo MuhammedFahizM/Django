333e42957e406e7418533e61e75ada8adcee5100
Muhammed Fahiz M <muhammedfahiz777145@gmail.com>
2025-11-14 12:34:18 +0530

    overriding with upper()

--- Model_View_Set/ModelViewSet_project/ModelViewSet_app/views.py
+++ Model_View_Set/ModelViewSet_project/ModelViewSet_app/views.py
@@ -24,13 +24,35 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 
+# class TaskViewSet(ModelViewSet):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerialzer
+
+#     @action(detail=False,methods=['get'])
+#     def completed(self,request):
+#         completed_tasks = Task.objects.filter(completed = True)
+#         serializer = self.get_serializer(completed_tasks,many = True)
+#         return Response(serializer.data) 
+
+
+from rest_framework import status
+
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerialzer
 
-    @action(detail=False,methods=['get'])
-    def completed(self,request):
-        completed_tasks = Task.objects.filter(completed = True)
-        serializer = self.get_serializer(completed_tasks,many = True)
-        return Response(serializer.data) 
+    def create(self, request, *args, **kwargs):
+        
+        # make request data mutable
+        data = request.data.copy()
+
+        # convert task name to uppercase
+        if 'task_name' in data:
+            data['task_name'] = data['task_name'].upper()
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception = True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
